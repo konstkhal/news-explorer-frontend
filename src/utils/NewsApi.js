6ef449c228951b8e459c2d9eeb9c98e91a3dc2ad
Konstantin Khaletsky <konstkhal@gmail.com>
2023-02-13 08:40:35 +0200

    feat(frontend): Add yandex proxy for api request

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -7,14 +7,22 @@ class NewsApi {
 
   _getFromDate = () => {
     const date = new Date();
-    this._fromDate = `${date.getFullYear()}-${date.getMonth() - this._fromOffset}-${date.getDate() + 1}`;
+    this._fromDate = `${date.getFullYear()}-${
+      date.getMonth() - this._fromOffset
+    }-${date.getDate() + 1}`;
   };
 
-  _handleResponse = res => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`));
+  _handleResponse = (res) =>
+    res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 
-  getNewsByQuery = param => {
-    return fetch(`${this._baseUrl}?q=${param}&from=${this._fromDate}&sortedBy=publishedAt&apiKey=${this._apiKey}`).then(this._handleResponse);
+  getNewsByQuery = (param) => {
+    return fetch(
+      `${this._baseUrl}?q=${param}&from=${this._fromDate}&sortedBy=publishedAt&apiKey=${this._apiKey}`
+    ).then(this._handleResponse);
   };
 }
 
-export const newsApi = new NewsApi('https://newsapi.org/v2/everything', '86ad54dd62e841a587816647ddf6c6d7');
+export const newsApi = new NewsApi(
+  "https://nomoreparties.co/news/v2/top-headlines?country=us",
+  "a3048e784a1a48f188b3c086840f45fb"
+);
