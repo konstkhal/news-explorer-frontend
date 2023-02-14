057fc4227b039287694282c62f156907bae6c740
Konstantin Khaletsky <konstkhal@gmail.com>
2023-02-14 08:16:29 +0200

    fix(frontend): Fix semantic header markup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,34 @@
-import './Header.css';
-import Logo from '../Logo/Logo';
-import Navbar from '../Navbar/Navbar';
-import { usePopups } from '../../contexts/PopupContext';
-import useWindowSize from '../../hooks/UseWindowSize';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useLocation } from 'react-router';
-import { MAX_MOBILE_SIZE } from '../../utils/constants';
+import "./Header.css";
+import Logo from "../Logo/Logo";
+import Navbar from "../Navbar/Navbar";
+import { usePopups } from "../../contexts/PopupContext";
+import useWindowSize from "../../hooks/UseWindowSize";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router";
+import { MAX_MOBILE_SIZE } from "../../utils/constants";
 
 const Header = () => {
   const [popupState] = usePopups();
-  const [headerClassName, setHeaderClassName] = useState('header');
+  const [headerClassName, setHeaderClassName] = useState("header");
   const isMobileSized = useWindowSize().width < MAX_MOBILE_SIZE;
-  const isSavedArticles = useLocation().pathname === '/saved-articles';
+  const isSavedArticles = useLocation().pathname === "/saved-articles";
 
   useEffect(() => {
     if (isMobileSized) {
-      setHeaderClassName(`header ${popupState.isUserMenuOpen ? 'header_dark' : ''}`);
+      setHeaderClassName(
+        `header ${popupState.isUserMenuOpen ? "header_dark" : ""}`
+      );
     } else {
-      setHeaderClassName('header');
+      setHeaderClassName("header");
     }
   }, [isMobileSized, popupState.isUserMenuOpen, isSavedArticles]);
 
   return (
-    <div className={headerClassName}>
+    <header className={headerClassName}>
       <Logo />
       <Navbar />
-    </div>
+    </header>
   );
 };
 
